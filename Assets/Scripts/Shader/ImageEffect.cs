6470acad1ecb1f06ccbf2b171947c1d0d03b83a0
marcmatija <marcmatija.mm@gmail.com>
2024-08-16 23:32:19 +0200

    [Color Palettes] Color is now handled using Scriptable Objects

--- Assets/Scripts/Shader/ImageEffect.cs
+++ Assets/Scripts/Shader/ImageEffect.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using ColorPalette = Color_Palettes.ColorPalette;
 
 namespace Shader
 {
@@ -8,7 +10,14 @@ namespace Shader
     {
         [SerializeField] private bool enabled;
         [SerializeField] private Material effectMaterial;
-    
+
+        [SerializeField] private ColorPalette _palette;
+
+        private void Awake()
+        {
+            effectMaterial.SetTexture(1,_palette.paletteTexture);
+        }
+
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
             if (effectMaterial)
