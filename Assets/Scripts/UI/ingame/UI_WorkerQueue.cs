16ab90d339b64463a69aa0343682046e8bc51d91
Lars Hurst <lars.hurst.lh1@gmail.com>
2024-08-18 16:19:43 +0200

    bILD NUIE

--- Assets/Scripts/UI/ingame/UI_WorkerQueue.cs
+++ Assets/Scripts/UI/ingame/UI_WorkerQueue.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using Color_Palettes;
 using Core;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using Workers;
 
 namespace UI.ingame
@@ -14,6 +17,12 @@ namespace UI.ingame
         private Queue<GameObject> _workers = new Queue<GameObject>();
         private GameObject _newWorker;
         [SerializeField] private float workerDistance = 20f;
+        public GameObject uIOutline;
+        public int outlineWorkerCount;
+        private Image _uIOutlineImage;
+        private float _iterator = 0;
+        public float iteratorPlus = 0.0001f;
+        private bool _uiuIOutlineAktie = false;
     
         void NewDudeInLine(WorkerColor workerColor)
         {
@@ -23,6 +32,11 @@ namespace UI.ingame
             _newWorker.GetComponent<UIQueueIcon>().SetIconColor(color);
             _newWorker.transform.SetSiblingIndex(_workers.Count);
             _workers.Enqueue(_newWorker);
+            if (_workers.Count >= outlineWorkerCount && !_uiuIOutlineAktie)
+            {
+                uIOutline.SetActive(true);
+                _iterator = 0;
+            }
         }
 
         void DeleteDudeInLine()
@@ -32,6 +46,12 @@ namespace UI.ingame
             foreach (var dude in _workers)
             {
                 dude.transform.position -= new Vector3(0, workerDistance, 0);
+            }
+
+            if (_workers.Count !>= outlineWorkerCount)
+            {
+                uIOutline.SetActive(false);
+                _uiuIOutlineAktie = false;
             }
         }
 
@@ -47,6 +67,16 @@ namespace UI.ingame
             WorkerManager.Instance.onNewWorkerSpaceRequested.AddListener(NewDudeInLine);
             WorkerManager.Instance.onWorkerQueueRelieved.AddListener(_ => DeleteDudeInLine());
             GameManager.Instance.GameOver.AddListener(OnGameOver);
+            uIOutline.SetActive(_workers.Count >= outlineWorkerCount);
+            _uIOutlineImage = uIOutline.GetComponent<Image>();
+        }
+
+        private void FixedUpdate()
+        {
+            _iterator += iteratorPlus;
+            if (_iterator > Mathf.PI) _iterator = 0;
+            _uIOutlineImage.color = new Color(1, 0, 0, 0.5f *Mathf.Sin(_iterator));
+            
         }
     }
 }
